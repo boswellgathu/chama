723116bff981cbd124ef5b166dd2188020d9edbd
boswellgathu <boswellgathu@gmail.com>
2022-05-20 19:10:51 +0300

    refactor saving+fine update

--- backend/app/crud/crud_saving.py
+++ backend/app/crud/crud_saving.py
@@ -51,37 +51,10 @@ class CRUDSaving(CRUDBase[Saving, SavingCreate, SavingUpdate]):
         else:
             update_data = obj_in.dict(exclude_unset=True)
 
-        date_sent_ = update_data.get("date_sent")
-        month_ = update_data.get("month")
-        if date_sent_ or month_:
-            date_sent, month = (
-                (date_sent_, db_obj.month)
-                if date_sent_ and (not month_)
-                else (db_obj.date_sent, month_)
-            )
-
-            is_saving_late: bool = self.is_saving_late(date_sent, month)
-
-            if is_saving_late != db_obj.is_late:
-                db_obj_id = db_obj.id
-                if is_saving_late and not db_obj.is_late:
-                    # is_saving_late = True and is_late = False
-                    # add a fine record
-                    fine_obj = self.create_fine(db, obj_in.member_id)
-                    update_data["fine_id"] = fine_obj.id
-                    update_data["is_late"] = True
-                    db_obj = self.get_by_id(db, id=db_obj_id)
-                elif db_obj.is_late and not is_saving_late:
-                    # is_saving_late = False and is_late = True
-                    # delete associated fine record
-                    fine_record = fine_class.get_by_id(db, id=db_obj.fine_id)
-                    db.delete(fine_record)
-                    db.commit()
-
-                    update_data["fine_id"] = None
-                    update_data["is_late"] = False
-                    db_obj = self.get_by_id(db, id=db_obj_id)
-
+        self.update_saving_and_related_fine_record(
+            db, update_data=update_data, db_obj=db_obj
+        )
+        db.refresh(db_obj)
         return super().update(db, db_obj=db_obj, obj_in=update_data)
 
     def is_saving_late(self, date_sent: date, saving_month: Month) -> bool:
@@ -98,8 +71,40 @@ class CRUDSaving(CRUDBase[Saving, SavingCreate, SavingUpdate]):
         fine = fine_class.create(db, obj_in=fine_data)
         return fine
 
-    def update_related_fine_record():
-        pass
+    def update_saving_and_related_fine_record(
+        self,
+        db: Session,
+        update_data: Union[SavingUpdate, Dict[str, Any]],
+        db_obj: Saving,
+    ):
+        date_sent_ = update_data.get("date_sent")
+        month_ = update_data.get("month")
+        if date_sent_ or month_:
+            date_sent, month = (
+                (date_sent_, db_obj.month)
+                if date_sent_ and (not month_)
+                else (db_obj.date_sent, month_)
+            )
+
+            is_saving_late: bool = self.is_saving_late(date_sent, month)
+
+            if is_saving_late != db_obj.is_late:
+                if is_saving_late and not db_obj.is_late:
+                    # is_saving_late = True and is_late = False
+                    # add a fine record
+                    fine_obj = self.create_fine(db, update_data["member_id"])
+                    update_data["fine_id"] = fine_obj.id
+                    update_data["is_late"] = True
+                elif db_obj.is_late and not is_saving_late:
+                    # is_saving_late = False and is_late = True
+                    # delete associated fine record
+                    fine_record = fine_class.get_by_id(db, id=db_obj.fine_id)
+                    db.delete(fine_record)
+                    db.commit()
+
+                    update_data["fine_id"] = None
+                    update_data["is_late"] = False
+        return update_data
 
 
 saving = CRUDSaving(Saving)
